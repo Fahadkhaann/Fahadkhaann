69e1161c3de96f3bdec652c2c2cdb08b987a94e1
Fahad Khan <102943563+Fahadkhaann@users.noreply.github.com>
2022-09-02 01:22:49 +0500

    Add files via upload

---
+++ q2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float gallons;
+    float cubicft;
+    cout<<"Enter number of gallons:\n"<<endl;
+    cin>>gallons;
+    cubicft= gallons/7.481;
+    cout<<"Equivalent in cubit feet is\n"<<cubicft<<endl;
+    return 0;
+
+
+
+    
+}
